2c86feef46f24b92305083049298307459a4f407
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-10-07 19:10:27 +0500

    Update ScrapeProxy.py

--- ScrapeProxy.py
+++ ScrapeProxy.py
@@ -7,10 +7,7 @@ proxy = f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 2
 # Настройка прокси в параметрах запроса
 while True:
     proxy = f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}:{random.randint(80, 60000)}"
-    proxies = {
-        'http': f'http://{proxy}',
-        'https': f'https://{proxy}'
-        }
+    proxies = {'http': f'http://{proxy}', 'https': f'https://{proxy}'}
     try:
         url = requests.get("https://google.com", proxies=proxies)
         # Дальнейшая обработка ответа от сервера
